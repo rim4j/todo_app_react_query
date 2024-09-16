23af30190eb9c04261ffcfdde9cca845da4d18a3
rim4j <amirhosseinjalali818@gmail.com>
2024-09-16 10:07:12 +0330

    edit task

--- src/components/SingleItem.tsx
+++ src/components/SingleItem.tsx
@@ -1,14 +1,29 @@
 import { Checkbox } from "@nextui-org/checkbox";
 import { Button } from "@nextui-org/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import customFetch from "@/utils/utils";
 
 const SingleItem = ({ singleItem }: any) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }: any) => {
+      return customFetch.patch(`/${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+
   return (
     <div className="flex justify-between mx-3 my-5">
       <div className="flex items-center ">
         <Checkbox
           lineThrough
-          onValueChange={(value) => console.log(value)}
-          // isSelected={singleItem.isDone}
+          isSelected={singleItem.isDone}
+          onValueChange={() =>
+            editTask({ taskId: singleItem.id, isDone: !singleItem.isDone })
+          }
         >
           <p>{singleItem.title}</p>
         </Checkbox>
